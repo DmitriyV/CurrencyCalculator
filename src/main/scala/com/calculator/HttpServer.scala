f34f3ab8dc2ae14e8a66a7386ec7be952d88e222
Dmitrii <dmitrii.volkov@derivco.ee>
2021-09-30 18:47:09 +0300

    set scala to version 2

--- src/main/scala/com/calculator/HttpServer.scala
+++ src/main/scala/com/calculator/HttpServer.scala
@@ -1,22 +1,33 @@
 package com.calculator
 
-import akka.actor.ActorSystem
-import akka.event.Logging
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.stream.ActorMaterializer
-import com.calculator.http.HealthRoute
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
 
-object HttpServer extends App {
+object HttpServer {
 
-  implicit val system: ActorSystem = ActorSystem("currency-calculator")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  def main(args: Array[String]): Unit = {
 
-  implicit val log = Logging(system, "main")
+    implicit val system = ActorSystem(Behaviors.empty, "my-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.executionContext
 
-  val port = 8080
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      }
 
-  val bindingFuture =
-    Http().bindAndHandle(HealthRoute.healthRoute, "localhost", port)
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
 
-  log.info(s"Server started at the port $port")
+    println(s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
 }
