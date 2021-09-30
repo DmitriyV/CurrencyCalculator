5825c7160f5348223c56a70fbb7f908bf4e011c6
Dmitrii <dmitrii.volkov@derivco.ee>
2021-09-30 18:56:08 +0300

    uncommented the tests

--- build.sbt
+++ build.sbt
@@ -9,5 +9,8 @@ val AkkaHttpVersion = "10.2.6"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
   "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
-  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "com.typesafe.akka" %% "akka-testkit" %  AkkaVersion,
+  "com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion,
+  "org.specs2" %% "specs2-core" % "4.5.1"
 )
--- src/main/scala/com/calculator/HttpServer.scala
+++ src/main/scala/com/calculator/HttpServer.scala
@@ -3,8 +3,8 @@ package com.calculator
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
+import com.calculator.http.HealthRoute
+
 import scala.io.StdIn
 
 object HttpServer {
@@ -15,16 +15,9 @@ object HttpServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
 
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
-      }
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(HealthRoute.healthRoute)
 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
-
-    println(s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop...")
+    println(s"Server now online. Please navigate to http://localhost:8080/health\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
