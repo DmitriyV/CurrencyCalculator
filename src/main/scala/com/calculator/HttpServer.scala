b712ea0a4d5d590a78f17a032159d3e82ed9788e
Dmitrii <dmitrii.volkov@derivco.ee>
2021-10-04 22:53:05 +0300

    Moved routes and added json support

--- src/main/scala/com/calculator/HttpServer.scala
+++ src/main/scala/com/calculator/HttpServer.scala
@@ -2,11 +2,25 @@ package com.calculator
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import com.calculator.http.HealthRoute
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport
+import io.circe.{Decoder, Encoder}
 
 import scala.concurrent.ExecutionContext
 
-trait Service {
+case class CurrencyPayload(from: Option[String], to: Option[String], value: BigDecimal)
+
+//marshaling/unmarshaling to JSON for CurrencyPayload
+trait Protocols extends ErrorAccumulatingCirceSupport {
+  import io.circe.generic.semiauto._
+
+  implicit val currencyRequestDecoder: Decoder[CurrencyPayload] = deriveDecoder
+  implicit val currencyRequestEncoder: Encoder[CurrencyPayload] = deriveEncoder
+}
+
+trait Service extends Protocols {
   implicit val system: ActorSystem
   // can execute program logic asynchronously
   implicit def executor: ExecutionContext
@@ -17,5 +31,20 @@ object HttpServer extends App with Service {
   override implicit val system: ActorSystem = ActorSystem()
   override implicit val executor: ExecutionContext = system.dispatcher
 
-  Http().newServerAt("localhost", 8080).bindFlow(HealthRoute.healthRoute)
+  val routes: Route =
+    path("health") {
+      get {
+        complete(StatusCodes.OK)
+      }
+    } ~
+      pathPrefix("eur") {
+        (get & path(Segment)) { ip =>
+          complete(StatusCodes.OK)
+        } ~
+          (post & entity(as[CurrencyPayload])) { currencyPayload =>
+            complete(StatusCodes.OK)
+          }
+      }
+
+  Http().newServerAt("localhost", 8080).bindFlow(routes)
 }
